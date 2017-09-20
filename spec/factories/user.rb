c712ab096554839e3b68ba3af71bfb3d30ab9adf
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-20 13:36:37 +0200

    fixed comments

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,7 +5,3 @@ FactoryGirl.define do
     password 'password'
     end
 end
-
-
-# let(:sender) { User.create(email: 'sender@test.com', password: 'password', name: 'sender') }
-# let(:receiver) { User.create(email: 'receiver@test.com', password: 'password', name: 'receiver') }
